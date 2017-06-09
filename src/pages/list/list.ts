d77e51a036a8e03f64250224e7151a2decd7ccf3
Michael Wakeling <michael@thirtytech.net>
2017-06-09 16:50:37 -0400

    workiung

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,15 +9,17 @@ import { Data } from "../../services/Data";
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{ title: string, note: string, icon: string }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public _data: Data) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-
+    this._data.db.child('/').on('value', data => {
+      console.log("Data: ", data.val());
+    });
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
     for (let i = 1; i < 11; i++) {
@@ -28,12 +30,12 @@ export class ListPage {
       });
     }
   }
-    ionViewDidLoad(){
-      console.log("Here");
-     this._data.db.child('/').on('value', data => {
-            console.log("Data: ",data.val());
-        });
-    }
+  ionViewDidLoad() {
+    console.log("Here");
+    this._data.db.child('/Test').on('value', data => {
+      console.log("Data: ", data.val());
+    });
+  }
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
     this.navCtrl.push(ListPage, {
